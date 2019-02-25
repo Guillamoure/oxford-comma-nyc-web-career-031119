17f1d231844143442e8dd945f7c632bd877505b4
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 03:44:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 def oxford_comma(array)
+  if array.length <= 1
+    puts array
+    elsif array.length == 2
+    
+  else
   last_word = array.last
   array.pop
   v = "and #{last_word}"
   array.push(v)
   array.join(", ")
+end
 end
